87409fc60ade5e5760bb4a1ef83038663c84e093
Jose Luis Contreras <joselawis@gmail.com>
2023-09-12 14:25:05 +0200

    First commit

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,17 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+// app.MapGet("/", () => "Hello World!");
+app.Run(async (HttpContext context) =>
+{
+    context.Response.StatusCode = 200;
+
+    context.Response.Headers["MyKey"] = "my value";
+    context.Response.Headers["Server"] = "My Server";
+
+    await context.Response.WriteAsync("Hello");
+    await context.Response.WriteAsync(" World!");
+});
+
+app.Run();
+

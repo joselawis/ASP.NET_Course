ae88b8f3a66cee6e415a7fde8f5ffd71bb09561b
Jose Luis Contreras <joselawis@gmail.com>
2023-09-12 17:26:03 +0200

    Last commit

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -1,11 +1,22 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.Extensions.Primitives;
+using Microsoft.AspNetCore.WebUtilities;
+
+var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 // app.MapGet("/", () => "Hello World!");
 app.Run(async (HttpContext context) =>
 {
-    System.IO.StreamReader reader = new StreamReader(context.Request.Body);
+    StreamReader reader = new StreamReader(context.Request.Body);
     string body = await reader.ReadToEndAsync();
+
+    Dictionary<string, StringValues> queryDict = QueryHelpers.ParseQuery(body);
+
+    if (queryDict.ContainsKey("firstName"))
+    {
+        string firstName = queryDict["firstName"][0];
+        await context.Response.WriteAsync(text: firstName);
+    }
 });
 
 app.Run();
